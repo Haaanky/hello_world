4e99223c22665944d87730473ce05e6df72ceca1
Henrik NF <Henriknorfri@gmail.com>
2019-03-10 11:28:16 +0100

    sure thing first commit

---
+++ newfile.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace NewShit
+{
+    class Program
+    {
+        public static void main (string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            TestMethod();
+        }
+        TestMethod()
+        {
+            System.Console.WriteLine("Goodbye World . . .");
+        }
+    }
+}
