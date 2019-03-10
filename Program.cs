6f0f45a0b63924d44154ac30b5fb104d72d20597
Henrik NF <Henriknorfri@gmail.com>
2019-03-10 11:35:34 +0100

    fourht commit added file, sigh

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace NewShit
+{
+    class Program
+    {
+        public static void main (string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            TestMethod();
+        }
+        static void TestMethod()
+        {
+            System.Console.WriteLine("Goodbye World . . .");
+        }
+    }
+}
